08badc3bf94e550d556722db3ee9afa2f9bffb09
Ofri Peretz <theoffricial@gmail.com>
2025-11-05 00:02:18 -0600

    chore: fix config file

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
-export default defineConfig(() => ({
+export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/playground',
   server: {
@@ -27,4 +27,4 @@ export default defineConfig(() => ({
       transformMixedEsModules: true,
     },
   },
-}));
+});
